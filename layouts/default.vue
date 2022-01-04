a80955f01f8ffebd8e584ab1392fbf4423696394
bobbykim89 <bobby.sihun.kim@gmail.com>
2022-01-04 01:02:07 -0700

    Initial files with minor changes

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Header />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: "Open Sans", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
