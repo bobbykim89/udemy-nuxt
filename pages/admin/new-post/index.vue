ef81d5e1749c26895011575a834490b7570f4c54
bobbykim89 <bobby.sihun.kim@gmail.com>
2022-01-05 18:35:09 -0700

    now executing adding and editing post through vuex store

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -16,15 +16,9 @@ export default {
   },
   methods: {
     onSubmitted(postData) {
-      axios
-        .post(
-          "https://udemy-nuxt-project-63b81-default-rtdb.firebaseio.com/posts.json",
-          { ...postData, updatedDate: new Date() }
-        )
-        .then((result) => {
-          this.$router.push("/admin");
-        })
-        .catch((e) => console.log(e));
+      this.$store.dispatch("addPost", postData).then(() => {
+        this.$router.push("/admin");
+      });
     },
   },
 };
--- store/index.js
+++ store/index.js
@@ -10,6 +10,15 @@ const createStore = () => {
       setPosts(state, posts) {
         state.loadedPosts = posts;
       },
+      addPost(state, post) {
+        state.loadedPosts.push(post);
+      },
+      editPost(state, editedPost) {
+        const postIndex = state.loadedPosts.findIndex(
+          (post) => post.id === editedPost.id
+        );
+        state.loadedPosts[postIndex] = editedPost;
+      },
     },
     actions: {
       nuxtServerInit(vuexContext, context) {
@@ -28,6 +37,34 @@ const createStore = () => {
       },
       setPosts(vuexContext, posts) {
         vuexContext.commit("setPosts", posts);
+      },
+      addPost(vuexContext, post) {
+        const createdPost = { ...post, updatedDate: new Date() };
+        return axios
+          .post(
+            "https://udemy-nuxt-project-63b81-default-rtdb.firebaseio.com/posts.json",
+            createdPost
+          )
+          .then((res) => {
+            vuexContext.commit("addPost", {
+              ...createdPost,
+              id: res.data.name,
+            });
+          })
+          .catch((e) => console.log(e));
+      },
+      editPost(vuexContext, editedPost) {
+        return axios
+          .put(
+            "https://udemy-nuxt-project-63b81-default-rtdb.firebaseio.com/posts/" +
+              editedPost.id +
+              ".json",
+            editedPost
+          )
+          .then((res) => {
+            vuexContext.commit("editPost", editedPost);
+          })
+          .catch((e) => console.log(e));
       },
     },
     getters: {
